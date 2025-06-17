342375d2e6de621625f1f401a1e66449bdc7fff1
ecomerce79 <fakeg9903@gmail.com>
2025-06-17 22:46:32 +0530

    OK

---
+++ Ecomerce-Project/src/main/java/com/nareshit/ecommerce/controller/Homecontroller.java
@@ -0,0 +1,17 @@
+package com.nareshit.ecommerce.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ecommerce")
+public class Homecontroller {
+
+    @GetMapping("/get")
+    public ResponseEntity<String> get(){
+        return ResponseEntity.ok("hello World") ;
+    }
+
+}
